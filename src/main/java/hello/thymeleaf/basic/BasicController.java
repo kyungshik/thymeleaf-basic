9a6c2bd35ff05d72e86fa574fa3d2103dedc7251
Kyungshik <kyungshikseong@gmail.com>
2024-03-22 16:27:39 +0900

    URL path variable query parameter

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -76,6 +76,14 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("link")
+    public String link(Model model) {
+        //https://ryan-han.com/post/translated/pathvariable_queryparam/
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Data
     static class User {
         private String username;
